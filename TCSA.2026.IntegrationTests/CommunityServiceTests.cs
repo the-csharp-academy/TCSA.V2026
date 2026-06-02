aaa2795db3d1487d89265d1c8a447b374b0b3966
Dejmenek <dejmen14567@gmail.com>
2026-06-02 12:07:45 +0200

    test: add integration tests to verify url processing behavior in CommunityServiceTests

--- TCSA.2026.IntegrationTests/CommunityServiceTests.cs
+++ TCSA.2026.IntegrationTests/CommunityServiceTests.cs
@@ -1,4 +1,7 @@
+using TCSA.V2026.Data.Enums;
 using TCSA.V2026.Data.Models;
+using TCSA.V2026.Data.Models.Responses;
+using TCSA.V2026.Helpers.Constants;
 using TCSA.V2026.Services;
 
 namespace TCSA.V2026.IntegrationTests;
@@ -24,8 +27,8 @@ public class CommunityServiceTests : IntegrationTestsBase
     [Test]
     public async Task IssuesShouldHaveNoDupes()
     {
-        await _service.CreateIssue(IssueType.Feature, "issueUrl.com", "Test Issue 1", "user1");
-        await _service.CreateIssue(IssueType.Feature, "issueUrl.com", "Test Issue 1", "user1");
+        await _service.CreateIssue(IssueType.Feature, $"{UrlConstants.TCSARepositoryIssuesUrl}/1", "Test Issue 1", CommunityProject.TCSA);
+        await _service.CreateIssue(IssueType.Feature, $"{UrlConstants.TCSARepositoryIssuesUrl}/1", "Test Issue 1", CommunityProject.TCSA);
 
         using var verifyContext = DbContextFactory.CreateDbContext();
 
@@ -34,5 +37,37 @@ public class CommunityServiceTests : IntegrationTestsBase
         .ToList();
 
         Assert.That(list.Count, Is.EqualTo(1));
+    }
+
+    [TestCase("https://github.com/random-org/random-repo/issues/1")]
+    [TestCase("https://github.com/the-csharp-academy/TCSA.V2026/pull/1")]
+    [TestCase("https://github.com/the-csharp-academy/TCSA.V2026/issues/")]
+    [TestCase("https://github.com/the-csharp-academy/TCSA.V2026/issues/abc")]
+    [TestCase("not-a-url")]
+    [TestCase("")]
+    public async Task IssueWithInvalidUrlShouldNotBeSaved(string invalidUrl)
+    {
+        var result = await _service.CreateIssue(IssueType.Feature, invalidUrl, "Invalid Issue", CommunityProject.TCSA);
+
+        using var verifyContext = DbContextFactory.CreateDbContext();
+        var count = verifyContext.Issues.Count();
+
+        Assert.That(result.Status, Is.EqualTo(ResponseStatus.Fail));
+        Assert.That(count, Is.EqualTo(0));
+    }
+
+    [TestCase("https://github.com/the-csharp-academy/TCSA.KnowTheCity/issues/1")]
+    [TestCase("https://github.com/the-csharp-academy/TCSA.V2026/issues/1")]
+    public async Task IssueWithValidUrlShouldBeSaved(string validUrl)
+    {
+        CommunityProject project = validUrl.Contains("KnowTheCity") ? CommunityProject.KnowTheCity : CommunityProject.TCSA;
+
+        var result = await _service.CreateIssue(IssueType.Feature, validUrl, "Valid Issue", project);
+
+        using var verifyContext = DbContextFactory.CreateDbContext();
+        var count = verifyContext.Issues.Count();
+
+        Assert.That(result.Status, Is.EqualTo(ResponseStatus.Success));
+        Assert.That(count, Is.EqualTo(1));
     }
 }
